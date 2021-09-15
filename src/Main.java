dcd5a7ab2acf6c9a9075df5c7946ee1318b7186e
krzys <krzysztofmuller1@gmail.com>
2021-09-15 23:23:12 +0200

    zadanie 1

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        /* Przykłady wyświetlania tekstu w konsoli */
+
         System.out.println("Witaj świecie!");
-        System.out.println("Po jest nasz pierwszy program w języku java");
-        System.out.println("Życzę" + " " + "wam " + "miłej nauki!");
+        System.out.println("Pierwszy projekt w javie");
+        System.out.println("Życzę" + "" + " miłego" + " dnia!");
 
-        System.out.printf("A %s zaczynamy z %d przykładem.\n", "więc", 1);
+        System.out.printf("No %s ruszamy z %d sposobem.\n", "to", 2);
+        System.out.printf("Kolejny %s z %d laborków.\n", "przykład", 1);
+        System.out.printf("To %s przyklad z %d sposobu.\n", "jest", 2);
 
-        System.out.print("To jest jakiś");
-        System.out.print(" tekst");
-        System.out.print(" nie za długi.");
-
-//        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
-//        Użyć każdej z opcji
-
+        System.out.print("Pełny");
+        System.out.print(" przykład");
+        System.out.print(" z 3 sposobu.");
     }
 }
